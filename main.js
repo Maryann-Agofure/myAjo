370f09f1e5ae4cf23ba258dd92e8a0b101716199
Maryann Agofure <maryannagofure@gmail.com>
2021-06-01 21:37:50 +0100

    adds bar menu

---
+++ main.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('.header-class')
+const barMenu = document.createElement('span')
+barMenu.innerHTML = `<span><i class="fas fa-bars bar-menu"></i></span>`
+header.appendChild(barMenu)
+
+
+
+
+
