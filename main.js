377278e2b826d5bf72af4727a3712744aae66d62
Maryann Agofure <maryannagofure@gmail.com>
2021-06-02 22:52:04 +0100

    adds a line

--- main.js
+++ main.js
@@ -3,7 +3,25 @@ const barMenu = document.createElement('span')
 barMenu.innerHTML = `<span><i class="fas fa-bars bar-menu"></i></span>`
 header.appendChild(barMenu)
 
+const menuNavigation = document.createElement('div')
+    
+    menuNavigation.innerHTML = `<div class = 'barDiv menuDiv'><nav class="nav-class">
+    <ul class="links">
+      <li><a href="#about">Home</a></li>
+      <li><a href="#services">Services</a></li>
+      <li><a href="#partners">Our Partners</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#onboard">Start process</a></li>
+    </ul>
+  </nav></div>`
+  header.appendChild(menuNavigation)
 
+function menuDisplay(){
+    barMenu.addEventListener('click', () => {
+menuNavigation.classList.toggle('menuDiv')
+    })
+}
+menuDisplay()
+ 
 
-
-
+    
