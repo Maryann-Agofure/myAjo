3425071e949d4db0d8b9a6038acae1193c8016c0
Maryann <maryannagofure@gmail.com>
2021-07-30 23:40:04 +0100

    back again

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
 const header = document.querySelector('.header-class')
-const barMenu = document.createElement('span')
-barMenu.innerHTML = `<span><i class="fas fa-bars bar-menu"></i></span>`
+
+
+function createBar(){
+  if(body.style.width <= '700px'){
+    const barMenu = document.createElement('span')
+    barMenu.innerHTML = `<span><i class="fas fa-bars bar-menu"></i></span>`
 header.appendChild(barMenu)
+barMenu.style.display = 'block'
+return 
+  }
+}
+createBar()
+
+
+
 
 const menuNavigation = document.createElement('div')
-    
-    menuNavigation.innerHTML = `<div class = 'barDiv menuDiv'><nav class="nav-class">
-    <ul class="links">
-      <li><a href="#about">Home</a></li>
-      <li><a href="#services">Services</a></li>
-      <li><a href="#partners">Our Partners</a></li>
-      <li><a href="#contact">Contact</a></li>
-      <li><a href="#onboard">Start process</a></li>
-    </ul>
-  </nav></div>`
-  header.appendChild(menuNavigation)
+ menuNavigation.setAttribute('class', 'barDiv')   
+      menuNavigation.innerHTML = `<div class ='menuDiv'><nav class="nav-class">
+      <ul class="links">
+        <li><a href="#about">Home</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#partners">Our Partners</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#onboard">Start process</a></li>
+      </ul>
+    </nav></div>`
+    barMenu.appendChild(menuNavigation)
+    menuNavigation.style.display = 'none'
 
 function menuDisplay(){
     barMenu.addEventListener('click', () => {
-menuNavigation.classList.toggle('menuDiv')
+      barDiv.style.display = 'block'
     })
 }
 menuDisplay()
